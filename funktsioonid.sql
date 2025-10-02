ee11a8ec7c881ecb9398827cd6e4e0c8b9f6673f
Roman Zaitsev <ihavearm0@gmail.com>
2025-10-02 16:27:05 +0300

    git käsudefail on loodud

---
+++ funktsioonid.sql
@@ -0,0 +1,13 @@
+﻿--я хочу домой очень((((
+select * from DimEmployee
+
+
+CREATE FUNCTION fn_ILTVF_GetEmployees()
+RETURNS TABLE
+AS
+RETURN
+(SELECT EmployeeKey, FirstName, 
+        CAST(BirthDate AS DATE) AS DOB
+    FROM dbo.DimEmployee
+);
+select * from fn_ILTVF_GetEmployees();
