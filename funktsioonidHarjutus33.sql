9b66d294634a8fc36ec8046edde48dd36a6e5fcd
Tefix <ihavearm0@gmail.com>
2025-10-06 09:44:00 +0300

    Lisatud on funktsioon fn_GetEmployeeNameById, mis tagastab töötaja nime tabelist DimEmployee.

---
+++ funktsioonidHarjutus33.sql
@@ -0,0 +1,11 @@
+select * from DimEmployee
+--- Esimine Funktsion
+Create Function fn_GetEmployeeNameById(@EmployeeKey int)
+
+Returns nvarchar(20)
+as
+Begin
+return (select FirstName from DimEmployee Where EmployeeKey = @EmployeeKey)
+End
+
+sp_helptext fn_GetEmployeeNameById
