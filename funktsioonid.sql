fd6dfee5cf9c0ba3b856dfe8bd43260857b8fb91
Tefix <ihavearm0@gmail.com>
2025-10-06 09:17:21 +0300

    Mitme avaldisega tabeliväärtusega funktsioonid e multi-statement table valued function

--- funktsioonid.sql
+++ funktsioonid.sql
@@ -10,3 +10,25 @@ RETURN
     FROM dbo.DimEmployee
 );
 select * from fn_ILTVF_GetEmployees();
+
+CREATE FUNCTION fn_MSTVF_GetEmployees()
+RETURNS @Table TABLE 
+(
+    EmloyeeKey INT, 
+    FirstName NVARCHAR(20), 
+    DOB DATE
+)
+AS
+BEGIN
+    INSERT INTO @Table
+    SELECT 
+        EmployeeKey, 
+        FirstName, 
+        CAST(BirthDate AS DATE)
+    FROM DimEmployee;
+
+    RETURN;
+END;
+
+SELECT * FROM fn_MSTVF_GetEmployees();
+
