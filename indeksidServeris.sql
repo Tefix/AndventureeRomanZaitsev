23e79c309e65252d46983dba1a6395cc08955a9f
Tefix <ihavearm0@gmail.com>
2025-10-06 11:48:51 +0300

    Lisatud ja uuesti loodud indeksid tabelile tblEmployee

--- indeksidServeris.sql
+++ indeksidServeris.sql
@@ -6,3 +6,17 @@ ON DimEmployee(BaseRate ASC)
 EXEC sp_help DimEmployee;
 -- Kui soovid kustutada indeksit
 DROP INDEX DimEmployee.IX_DimEmployee_BaseRate
+
+-- Loob klasterindeksi veeru FirstName järgi, et kiirendada otsingut ja sortimist
+CREATE CLUSTERED INDEX IX_tblEmployee_Name 
+ON tblEmployee(FirstName);
+
+DROP INDEX tblEmployee.PK__tblEmplo__3214EC070A9D95DB;
+
+-- Loob klasterindeksi veergude Gender ja Salary järgi, et optimeerida päringuid nende väljade alusel
+CREATE CLUSTERED INDEX IX_tblEmployee_Gender_salary
+ON tblEmployee(Gender DESC, Salary ASC);
+
+-- Loob mitteklasterindeksi veeru FirstName järgi, et kiirendada otsingut ilma andmete salvestusjärjestust muutmata
+CREATE NONCLUSTERED INDEX IX_tblEmployee_Name 
+ON tblEmployee(FirstName);
