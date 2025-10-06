8628781d384be960b20089bd24faf05a92d52982
Tefix <ihavearm0@gmail.com>
2025-10-06 09:54:07 +0300

    Tabeli fn_GetEmployeeNameById krüpteerimise lisamine

--- funktsioonidHarjutus33.sql
+++ funktsioonidHarjutus33.sql
@@ -9,4 +9,14 @@ return (select FirstName from DimEmployee Where EmployeeKey = @EmployeeKey)
 End
 
 sp_helptext fn_GetEmployeeNameById
------
+-----alter function 
+Alter Function fn_GetEmployeeNameById(@EmployeeKey int)
+
+Returns nvarchar(20)
+With Encryption 
+as
+begin
+Return (select FirstName from DimEmployee Where EmployeeKey = @EmployeeKey)
+End
+
+sp_helptext fn_GetEmployeeNameById
