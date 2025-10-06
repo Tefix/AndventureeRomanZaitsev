c83abdb14fddcffe625f9967bc2fb3ecd796f4ea
Tefix <ihavearm0@gmail.com>
2025-10-06 12:32:11 +0300

    lisame except operatsioonid

---
+++ SqlExcept.sql
@@ -0,0 +1,21 @@
+USE AdventureWorksDW2019;
+
+select * from DimEmployee
+select * from DimCustomer
+
+-- Except operaator tagastab unikaalsed read vasakust päringust.
+SELECT EmployeeKey, FirstName, Gender
+FROM DimEmployee
+EXCEPT
+SELECT CustomerKey, FirstName, Gender
+FROM DimCustomer
+
+-- Order by nõuet võib kasutada ainult kord peale paremat päringut
+SELECT EmployeeKey, FirstName, Gender, BaseRate
+FROM DimEmployee
+WHERE BaseRate >= 10
+EXCEPT
+SELECT CustomerKey, FirstName, Gender, YearlyIncome
+FROM DimCustomer
+WHERE YearlyIncome >= 20000
+ORDER BY FirstName
