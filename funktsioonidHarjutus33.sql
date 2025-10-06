b05a2ef560321ee3b3598e36352d32b70457e470
Tefix <ihavearm0@gmail.com>
2025-10-06 10:22:26 +0300

    Funktsiooni muutmine kasutades SCHEMABINDINGut

--- funktsioonidHarjutus33.sql
+++ funktsioonidHarjutus33.sql
@@ -20,3 +20,15 @@ Return (select FirstName from DimEmployee Where EmployeeKey = @EmployeeKey)
 End
 
 sp_helptext fn_GetEmployeeNameById
+
+----Alter Function 
+Alter Function fn_GetEmployeeNameById(@EmployeeKey int)
+Returns vachar(20)
+With SchemaBinding
+as
+Begin
+Return (Select FirstName from DimEmployee where EmployeeKey = @EmployeeKey)
+end
+ 
+ 
+ DROP TABLE tblEmployees
