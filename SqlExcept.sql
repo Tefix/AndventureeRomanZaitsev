49fc8253dd4134139bb0d98b2c61029b56ea87d5
Tefix <ihavearm0@gmail.com>
2025-10-09 14:03:44 +0300

    -- Erinevus Except ja not in operaatoril

--- SqlExcept.sql
+++ SqlExcept.sql
@@ -20,3 +20,33 @@ FROM DimCustomer
 WHERE YearlyIncome >= 20000
 ORDER BY FirstName
 
+-- Erinevus Except ja not in operaatoril
+-- Sama tulemuse võib saavutada NOT IN operaatoriga:
+SELECT EmployeeKey, FirstName, Gender
+FROM DimEmployee
+WHERE EmployeeKey NOT IN (SELECT CustomerKey FROM DimCustomer)
+
+-- Sisesta järgnev rida tabelisse TableA
+INSERT INTO DimEmployee VALUES (1, 'Mark', 'Male')
+-- Nüüd käivita järgnev EXCEPT päring
+SELECT EmployeeKey, FirstName, Gender
+FROM DimEmployee
+EXCEPT
+SELECT CustomerKey, FirstName, Gender
+FROM DimCustomer
+-- Nüüd käivita NOT IN operaatoriga kood
+SELECT EmployeeKey, FirstName, Gender
+FROM DimEmployee
+WHERE EmployeeKey NOT IN (SELECT CustomerKey FROM DimCustomer)
+
+-- Järgnevas päringus on meelega veergude arv erinev
+SELECT EmployeeKey, FirstName, Gender
+FROM DimEmployee
+EXCEPT
+SELECT CustomerKey, FirstName
+FROM DimCustomer
+
+-- Järgnevas päringus alampäring tagastab mitu veergu
+SELECT EmployeeKey, FirstName, Gender
+FROM DimEmployee
+WHERE EmployeeKey NOT IN (SELECT CustomerKey, FirstName FROM DimCustomer)
